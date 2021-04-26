1f71d002e872b7d350ff6f10cb19234e1e62fe59
dijiflex <dijiflex@gmail.com>
2021-04-26 08:39:22 +0300

    setup the sidebar component

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAL3QA5EI11tDZO2xp9qWtOMzG7zNLbjK4",
+    authDomain: "whatsapp-clone-693f1.firebaseapp.com",
+    projectId: "whatsapp-clone-693f1",
+    storageBucket: "whatsapp-clone-693f1.appspot.com",
+    messagingSenderId: "164190501240",
+    appId: "1:164190501240:web:05a1317fc4e1e495b2b08d",
+    measurementId: "G-54TJH1RQH6"
+  };
