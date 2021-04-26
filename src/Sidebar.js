80ebf6a4bcf84e8cba9c3c050f9866de9fb249a7
dijiflex <dijiflex@gmail.com>
2021-04-26 10:27:20 +0300

    Finished header section ui

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import {Avatar, IconButton}  from "@material-ui/core";
+import './Sidebar.css'
 
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 function Sidebar() {
     return (
         <div className='sidebar'>
-            <h1>Sidebar</h1>
+            <div className='sidebar__header'>
+                <IconButton>
+                <Avatar/>
+                </IconButton>
+                
+                <div className='sidebar__headerRight'>
+                <IconButton>
+                    <DonutLargeIcon />
+                </IconButton>
+                <IconButton>
+                    <ChatIcon />
+                </IconButton>
+                <IconButton>
+                    <MoreVertIcon />
+                </IconButton>
+                    
+                </div>
+
+            </div>
+            <div className='sidebar__search'>
+
+            </div>
+            <div className='sidebar__chats'>
+
+            </div>
         </div>
     )
 }
