cf638249223935f42e1fa19e8e5d1208035cafde
dijiflex <dijiflex@gmail.com>
2021-05-02 22:40:46 +0300

    rendered the last message on the sidebar

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -4,11 +4,23 @@ import { Avatar } from '@material-ui/core'
 import db from './firebase'
 import {Link} from 'react-router-dom'
 function SidebarChat({addNewChat, id, name }) {
-    const [seed, setSeed] = useState('')
+    const [seed, setSeed] = useState('');
+const [message, setMessage] = useState('')
+
     useEffect(() => {
        setSeed(Math.floor(Math.random()*5000)) 
         
     }, []);
+    useEffect(() => {
+        if(id) {
+            db.collection('rooms').doc(id).collection('messages').orderBy('timestamp', 'desc').onSnapshot(
+                snapshot => (setMessage(snapshot.docs.map(doc => doc.data())))
+            )
+        }
+        return () => {
+            // cleanup
+        }
+    }, [])
 
     const createChat = () => {
         const roomName = prompt('Please enter name for chat');
@@ -27,7 +39,7 @@ function SidebarChat({addNewChat, id, name }) {
             <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
             <div className='sidebarChat__info'>
                 <h2>{name}</h2>
-                <p>Last message .... </p>
+                <p>{message[0]?.message}</p>
 
             </div>
         </div>
